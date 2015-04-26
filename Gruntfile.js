963a4582d6f324e5d04ae0ffa5aa7dca27add73f
Daniel Perez Alvarez <unindented@gmail.com>
2015-04-26 12:28:26 +0100

    Stop grunt from opening my browser

--- Gruntfile.js
+++ Gruntfile.js
@@ -96,8 +96,7 @@ module.exports = function(grunt) {
 				options: {
 					port: port,
 					base: '.',
-                    livereload: true,
-                    open: true
+						livereload: true
 				}
 			}
 		},
@@ -114,9 +113,9 @@ module.exports = function(grunt) {
 		},
 
 		watch: {
-            options: {
-                livereload: true
-            },
+			options: {
+				livereload: true
+			},
 			js: {
 				files: [ 'Gruntfile.js', 'js/reveal.js' ],
 				tasks: 'js'
@@ -129,9 +128,9 @@ module.exports = function(grunt) {
 				files: [ 'css/reveal.scss' ],
 				tasks: 'css-core'
 			},
-            html: {
-                files: [ 'index.html']
-            }
+			html: {
+				files: [ 'index.html']
+			}
 		}
 
 	});
